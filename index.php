42e7a62746781b62d577cfa7e88d6c80fcf6ad63
James Biltaji <jimilordofwar@hotmail.com>
2019-03-09 20:06:43 +0000

    added the dragonhead dataset provided by Riot

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <! DOCTYPE html>
 <html>
+	<head>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="/public/bin/css/main.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	</head>
 	<body>
-	<?php
-	$test = "this is a php string.";
-	?>
-		<h2>Hello World<h2>
-		<p>Update Test</p>
-		<p>test 2</p>
-		<p>test 3</p>
-		<p><?php echo $test?>...</p>
+		<div class="container">
+			<?php $test = "this is a php string."; ?>
+				<h2>Hello World<h2>
+				<p>Update Test</p>
+				<p>test 2</p>
+				<p>test 3</p>
+				<p><?php echo $test?>...</p>
+
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		</div>
 	</body>
 </html>
